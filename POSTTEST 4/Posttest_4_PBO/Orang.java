12aefb3ec6e2b3e0fa1975cbccd6e1582dabef14
MuhammadMuchlisAbimanyu <92872429+MuhammadMuchlisAbimanyu@users.noreply.github.com>
2023-03-29 14:51:13 +0800

    Add files via upload

---
+++ POSTTEST 4/Posttest_4_PBO/Orang.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Posttest_4_PBO;
+
+/**
+ *
+ * @author Grylz
+ */
+public class Orang {
+    protected String nama;
+    protected int umur;
+    
+    public Orang(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    public void display(){
+        System.out.println("Nama Dokter     : " + this.nama);
+        System.out.println("Umur Dokter     : " + this.umur);
+    }
+    
+}
